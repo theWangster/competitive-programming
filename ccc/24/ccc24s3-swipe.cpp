1947bae5a3a99ddacab42066d98aca91e539c8a5
theWangster <mr.xywang@gmail.com>
2024-02-21 11:39:38 -0500

    holy shit i fucked up the cc

--- ccc/24/ccc24s3-swipe.cpp
+++ ccc/24/ccc24s3-swipe.cpp
@@ -20,6 +20,14 @@ int findNextNum(vector<int>& start, int size, int idx, int target) {
 	return -1;
 }
 
+int findNextNumLeft(vector<int>& start, int size, int idx, int target) {
+	for (int i = idx; i >= 0; --i) {
+		if (start[i] == target) {
+			return i;
+		}
+	}
+	return -1;
+}
 void printArr(vector<int>& start) {
 	for (int temp : start) {
 		cout << temp << " ";
@@ -27,7 +35,8 @@ void printArr(vector<int>& start) {
 	cout << endl;
 }
 
-bool nextMove(vector<int>& start, vector<int>& end, vector<string>& actions, int size, int idx) {
+bool nextMove(vector<int>& start, vector<int>& end, vector<string>& actions, int size, int idx,
+			  bool right) {
 	/*
 	cout << idx << ":" << endl;
 	printArr(start);
@@ -36,30 +45,57 @@ bool nextMove(vector<int>& start, vector<int>& end, vector<string>& actions, int
 		return checkCorrect(start, end, size);
 	}
 
-	int cur = start[idx];
-	int target = end[idx];
-	if (cur != target) {
-		if (idx - 1 >= 0 && start[idx - 1] == target) {
-			// cout << "DO THIS SHIT" << endl;
-			string nextAction = "R " + to_string(idx - 1) + " " + to_string(idx);
+	if (right) {
+		int cur = start[idx];
+		int target = end[idx];
+		if (cur != target) {
+			if (idx - 1 >= 0 && start[idx - 1] == target) {
+				// cout << "DO THIS SHIT" << endl;
+				string nextAction = "R " + to_string(idx - 1) + " " + to_string(idx);
+				actions.push_back(nextAction);
+				start[idx] = target;
+				return nextMove(start, end, actions, size, idx + 1, false);
+			}
+
+			int nextNum = findNextNum(start, size, idx, target);
+			if (nextNum == -1) {
+				return false;
+			}
+			for (int i = idx; i < nextNum; ++i) {
+				start[i] = target;
+			}
+			string nextAction = "L " + to_string(idx) + " " + to_string(nextNum);
 			actions.push_back(nextAction);
-			start[idx] = target;
-			return nextMove(start, end, actions, size, idx + 1);
+			return nextMove(start, end, actions, size, idx + 1, false);
 		}
 
-		int nextNum = findNextNum(start, size, idx, target);
-		if (nextNum == -1) {
-			return false;
+		return nextMove(start, end, actions, size, idx + 1, false);
+	} else {
+		int idx_new = size - idx;
+		int cur = start[idx_new];
+		int target = end[idx_new];
+		if (cur != target) {
+			if (idx_new + 1 < size && start[idx_new + 1] == target) {
+				string nextAction = "L " + to_string(idx_new) + " " + to_string(idx_new + 1);
+				actions.push_back(nextAction);
+				start[idx_new] = target;
+				return nextMove(start, end, actions, size, idx, true);
+			}
+
+			int nextNum = findNextNumLeft(start, size, idx_new, target);
+			if (nextNum == -1) {
+				return false;
+			}
+			for (int i = nextNum; i <= idx_new; ++i) {
+				start[i] = target;
+			}
+			string nextAction = "R " + to_string(nextNum) + " " + to_string(idx_new);
+			actions.push_back(nextAction);
+			return nextMove(start, end, actions, size, idx, true);
 		}
-		for (int i = idx; i < nextNum; ++i) {
-			start[i] = target;
-		}
-		string nextAction = "L " + to_string(idx) + " " + to_string(nextNum);
-		actions.push_back(nextAction);
-		return nextMove(start, end, actions, size, idx + 1);
+
+		return nextMove(start, end, actions, size, idx, true);
 	}
-
-	return nextMove(start, end, actions, size, idx + 1);
 }
 
 int main() {
@@ -90,7 +126,7 @@ int main() {
 		return 0;
 	}
 
-	if (nextMove(start, end, actions, arrSize, 0)) {
+	if (nextMove(start, end, actions, arrSize, 0, true)) {
 		cout << "YES" << endl;
 		cout << actions.size() << endl;
 		for (string action : actions) {
